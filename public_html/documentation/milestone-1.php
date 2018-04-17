d7de59763ac51f8003aabda182333336442719a0
James Ticer <jticer4@icloud.com>
2018-04-16 19:22:00 -0600

    added my elements under persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,10 +6,18 @@
 	</head>
 	<body>
 		<h3>Purpose:</h3>
-		<p>I want my PWP to serve as a portfolio so that I am able to market myself to employers after bootcamp.</p>
+			<p>I want my PWP to serve as a portfolio so that I am able to market myself to employers after bootcamp.</p>
 		<h3>Audience:</h3>
-		<p>The audience for my site will be potential employers.</p>
+			<p>The audience for my site will be potential employers.</p>
 		<h3>Goal:</h3>
-		<p>To acquire full-time employment. </p>
+			<p>To acquire full-time employment. </p>
+		<h3>Persona</h3>
+			<h5>Name:</h5>
+			<h5>Gender:</h5>
+			<h5>Age:</h5>
+			<h5>Technology:</h5>
+			<h5>Attitudes:</h5>
+			<h5>Goals:</h5>
+			<h5>Frustrations:</h5>
 	</body>
 </html>
