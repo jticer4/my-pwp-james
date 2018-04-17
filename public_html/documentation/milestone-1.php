579a4064267e272f000a3ede8cd1270f7cad089e
James Ticer <jticer4@icloud.com>
2018-04-17 17:30:56 -0600

    added some more content to purpose, audience, goal, and persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,11 +7,11 @@
 	<body>
 		<h1>My Pwp James</h1>
 		<h3>Purpose:</h3>
-			<p>I want my PWP to serve as a portfolio so that I am able use to market myself to employers after bootcamp.</p>
+			<p>I want my PWP to serve as a portfolio I can use to market myself to employers after boot camp is finished.</p>
 		<h3>Audience:</h3>
-			<p>The audience for my site will be potential employers.</p>
+			<p>The audience for my site will be potential tech employers.</p>
 		<h3>Goal:</h3>
-			<p>To acquire full-time employment. </p>
+			<p>To acquire full-time employment as soon as possible. </p>
 		<h2>Persona</h2>
 			<h4>Name:</h4>
 				<p>Pamela Slanderson</p>
@@ -22,12 +22,14 @@
 			<h4>Technology:</h4>
 				<p>iPhone 7 running iOS 11 and an HP Envy running the latest version of Windows 10.</p>
 			<h4>Attitudes:</h4>
-				<p>Pamela is part of the Human Resources Department and doesn't know very much about designing software. She is
+				<p>Pamela is part of the Human Resources Department at Scoutible and doesn't know anything about writing code. She is
 					impatient and doesn't want to learn software development in order to find the right employee.</p>
 			<h4>Behaviors:</h4>
-				<p>Pamela has been reading resumes all day and is starting to become frustrated.</p>
+				<p>Pamela has been reading resumes all day and is starting to become frustrated. She has gotten so impatient
+				that she is now rushing through applicants waiting for someone to stand out from the crowd.</p>
 			<h4>Goal:</h4>
-				<p>Without knowing too much, Pamela wants to feel confident hiring somebody who knows what they're doing.</p>
+				<p>Pamela wants to hire somebody who knows what they're doing. Regardless of her coding skills, or lack there of,
+				she wants to feel confident that she has made the right decision.</p>
 			<h4>Frustrations:</h4>
 				<ul>
 					<li>Hiring employees who are unable/unwilling to learn.</li>
