54afb1c66741fba93786059ac505ce1c12e05883
James Ticer <jticer4@icloud.com>
2018-04-16 19:52:47 -0600

    added content to my personas

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,11 +13,26 @@
 			<p>To acquire full-time employment. </p>
 		<h3>Persona</h3>
 			<h5>Name:</h5>
+				<p></p>
 			<h5>Gender:</h5>
+				<p>Female</p>
 			<h5>Age:</h5>
+				<p>45 years old</p>
 			<h5>Technology:</h5>
+				<p>iPhone 7 running iOS 11 and an HP Envy running the latest version of Windows 10.</p>
 			<h5>Attitudes:</h5>
-			<h5>Goals:</h5>
+				<p>______ is part of the Human Resources Department and doesn't know very much about designing software._____ is
+				impatient and doesn't want to learn software development in order to find the right employee.
+			<h5>Behaviors:</h5>
+				<p>______ has been reading resumes all day and is starting to become frustrated.</p>
+			<h5>Goal:</h5>
+				<p>Without knowing too much, ______wants to feel confident hiring somebody who knows what they're doing.</p>
 			<h5>Frustrations:</h5>
+				<ul>
+					<li>Hiring employees who are unable/unwilling to learn.</li>
+					<li>Seeing candidates who don't have a solid portfolio.</li>
+					<li>Interviewing candidates who are unable to work as a team.</li>
+					<li>Candidates who don't have specific experience with the languages used by the company.</li>
+				</ul>
 	</body>
 </html>
