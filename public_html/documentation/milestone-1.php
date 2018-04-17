b7b2b54e02832b33e087c1dd2e06815f654b9dae
James Ticer <jticer4@icloud.com>
2018-04-16 20:10:30 -0600

    changed my h5 tags to h4 tags

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -12,22 +12,22 @@
 		<h3>Goal:</h3>
 			<p>To acquire full-time employment. </p>
 		<h3>Persona</h3>
-			<h5>Name:</h5>
+			<h4>Name:</h4>
 				<p></p>
-			<h5>Gender:</h5>
+			<h4>Gender:</h4>
 				<p>Female</p>
-			<h5>Age:</h5>
+			<h4>Age:</h4>
 				<p>45 years old</p>
-			<h5>Technology:</h5>
+			<h4>Technology:</h4>
 				<p>iPhone 7 running iOS 11 and an HP Envy running the latest version of Windows 10.</p>
-			<h5>Attitudes:</h5>
+			<h4>Attitudes:</h4>
 				<p>______ is part of the Human Resources Department and doesn't know very much about designing software._____ is
 				impatient and doesn't want to learn software development in order to find the right employee.
-			<h5>Behaviors:</h5>
+			<h4>Behaviors:</h4>
 				<p>______ has been reading resumes all day and is starting to become frustrated.</p>
-			<h5>Goal:</h5>
+			<h4>Goal:</h4>
 				<p>Without knowing too much, ______wants to feel confident hiring somebody who knows what they're doing.</p>
-			<h5>Frustrations:</h5>
+			<h4>Frustrations:</h4>
 				<ul>
 					<li>Hiring employees who are unable/unwilling to learn.</li>
 					<li>Seeing candidates who don't have a solid portfolio.</li>
