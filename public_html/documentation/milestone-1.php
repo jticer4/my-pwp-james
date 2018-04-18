9285d868b9a4bcb4f0b3e3ddbf83af1d6db33b15
James Ticer <jticer4@icloud.com>
2018-04-18 10:25:46 -0600

    added some more content to make my persona more specific

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,33 +9,40 @@
 		<h3>Purpose:</h3>
 			<p>I want my PWP to serve as a portfolio I can use to market myself to employers after boot camp is finished.</p>
 		<h3>Audience:</h3>
-			<p>The audience for my site will be potential tech employers.</p>
+			<p>The audience for my site will be Technical Leads from potential employers. I'm interested in attracting attention
+				from small start-ups.</p>
 		<h3>Goal:</h3>
-			<p>To acquire full-time employment as soon as possible. </p>
+			<p>To demonstrate my technical expertise to Technical Leads and Internal Recruiters, with the goal of getting hired as a Web Developer. </p>
 		<h2>Persona</h2>
 			<h4>Name:</h4>
-				<p>Pamela Slanderson</p>
+				<p>Pamela Swanson</p>
 			<h4>Gender:</h4>
 				<p>Female</p>
 			<h4>Age:</h4>
-				<p>45 years old</p>
+				<p>32 years old</p>
 			<h4>Technology:</h4>
 				<p>iPhone 7 running iOS 11 and an HP Envy running the latest version of Windows 10.</p>
 			<h4>Attitudes:</h4>
-				<p>Pamela is part of the Human Resources Department at Scoutible and doesn't know anything about writing code. She is
-					impatient and doesn't want to learn software development in order to find the right employee.</p>
+				<p>Pamela is a Technical Lead at a Scoutible. She only wants to interview people who have a well rounded
+				understanding of the role played by a Web Developer. She really appreciates it when people ask the right questions.
+				She wants the person she hires to be personable because she feels like life is too short to work with people you
+				don't like.</p>
 			<h4>Behaviors:</h4>
 				<p>Pamela has been reading resumes all day and is starting to become frustrated. She has gotten so impatient
-				that she is now rushing through applicants waiting for someone to stand out from the crowd.</p>
+				that she is now rushing through applicants waiting for someone to stand out from the crowd. She tends to notice
+				people who carry a positive attitude and clearly demonstrate that they enjoy their work. She tends to remember the
+				people who follow up after the interview.</p>
 			<h4>Goal:</h4>
-				<p>Pamela wants to hire somebody who knows what they're doing. Regardless of her coding skills, or lack there of,
-				she wants to feel confident that she has made the right decision.</p>
+				<p>Pamela wants to hire somebody who knows what they're doing and is excited to work at Scoutible. She wants to feel confident that she has made the right decision.
+				Her goal is to find someone who can hit the ground running, and is able to contribute right away.</p>
 			<h4>Frustrations:</h4>
 				<ul>
 					<li>Hiring employees who are unable/unwilling to learn.</li>
 					<li>Seeing candidates who don't have a solid portfolio.</li>
 					<li>Interviewing candidates who are unable to work as a team.</li>
 					<li>Candidates who don't have specific experience with the languages used by the company.</li>
+					<li>Hiring people who are not a good fit and having to go back to the drawing board. </li>
+					<li>Searching through a website to find the information she's looking for.</li>
 				</ul>
 	</body>
 </html>
