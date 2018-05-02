ab8b6495cdcedd25860a4cb98478c983fab17bd9
James Ticer <jticer4@icloud.com>
2018-05-02 16:31:36 -0600

    added in my milestone 2.php file and added my content strategy into it

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p><strong>My site will be one page, and will consist of the following sections:</strong></p>
+		<h3>Overall</h3>
+		<p>I want to include my favorite quotes(or quotes about me) below each of the main headings.(these quotes will
+			serve to address common concerns that technical leads or recruiters may have)</p>
+		<h3>Welcome</h3>
+		<p>A brief one-line introduction that clearly states my name and role as a Software Developer. This welcome
+			message will be overlaid on a large background image of the island I grew up on, Kauai.
+			Additionally, I will include sticky links to my Git, LinkedIn, Twitter, and Email, in the top right corner.
+			I will also include a drop down menu in the top left corner that will allow users to quickly navigate to the
+			main sections of my site. This drop down menu will stay fixed in the top left corner as the user scrolls
+			through the page. Lastly, there will be a tiny arrow at the very bottom, prompting the user to scroll down.
+		</p>
+		<h3>Profile</h3>
+		<p>This section will consist of an "about me" & "details" section. The about me section will provide a quick high
+			level overview of my most notable accomplishments, and will be written in my own "voice". The details section
+			will provide some basic information about me, such as, my name, age, location, and a button to follow me on
+			twitter. This section will also include an image of my professional headshot.
+		</p>
+		<h3>Experience</h3>
+		<p>This section will consist of my education, experience, and projects. I will first briefly summarize my
+			educational and training history. Then I will provide an overall look at my experience and projects, with the
+			goal being to showcase the quality of my work. Each achievement will include a quick summary below a small
+			screenshot of what is being summarized.
+		</p>
+		<h3>Abilities and Tools</h3>
+		<p>This section will highlight my technical expertise and will offer more information as to how familiar I am with
+			each Language or Tool. This information will be displayed as a one to five star rating.
+		</p>
+		<h3>Contact</h3>
+		<p>This will be my last section and will include a set of links to my Git hub, LinkedIn, Twitter, and email. When
+			the user clicks on the email logo it will automatically open their default email application and auto populate
+			my email address into the "to" section of the email.
+		</p>
+	</body>
+</html>
