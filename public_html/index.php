27a98c7d02ac2877edeca4c98dc168576d01a16f
James Ticer <jticer4@icloud.com>
2018-05-16 16:17:39 -0600

    added in some font awesome icons

--- public_html/index.php
+++ public_html/index.php
@@ -7,6 +7,9 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
+		<!-- Import Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -18,7 +21,21 @@
 	<body>
 		<div class="container">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<a class="navbar-brand" href="#">Navbar</a>
+				<a class="navbar-brand" href="#">James Ticer</a>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item  fab fa-github">
+						<a class="nav-link" href="#">
+					</li>
+					<li class="nav-item fab fa-linkedin-in">
+						<a class="nav-link" href="#">
+					</li>
+					<li class="nav-item fab fa-twitter">
+						<a class="nav-link" href="#">
+					</li>
+					<li class="nav-item fas fa-envelope">
+						<a class="nav-link" href="#">
+					</li>
+				</ul>
 			</nav>
 		</div>
 	</body>
