20cb274a84b292ae2ce1f081a22cf47325820082
James Ticer <jticer4@icloud.com>
2018-05-16 15:57:12 -0600

    made some aesthetic changes to my nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -17,7 +17,7 @@
 	</head>
 	<body>
 		<div class="container">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 				<a class="navbar-brand" href="#">Navbar</a>
 			</nav>
 		</div>
