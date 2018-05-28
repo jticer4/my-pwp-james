b0a4378dddcb22bb3a4e9699558b7d69c2bbc914
James Ticer <jticer4@icloud.com>
2018-05-28 13:07:02 -0600

    refactored my contact form

--- public_html/index.php
+++ public_html/index.php
@@ -204,33 +204,64 @@
 		<div class="container-fluid text-center">
 			<h3 class="mt-5" id="contact">Contact Me</h3>
 			<hr class="pb-3">
-			<form>
-				<div class="text-center py-1">
-					<input type="text" placeholder="Name:">
+			<form id="contact-form" method="post" action="placeholder.php" role="form">
+
+
+				<div class="controls">
+
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="form_name">First Name *</label>
+								<input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your first name *" required="required" data-error="First name is required.">
+								<div class="help-block with-errors"></div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="form_lastname">Last Name *</label>
+								<input id="form_lastname" type="text" name="surname" class="form-control" placeholder="Please enter your last name *" required="required" data-error="Last name is required.">
+								<div class="help-block with-errors"></div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="form_email">Email *</label>
+								<input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
+								<div class="help-block with-errors"></div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label for="form_phone">Phone</label>
+								<input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
+								<div class="help-block with-errors"></div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="form-group">
+								<label for="form_message">Message *</label>
+								<textarea id="form_message" name="message" class="form-control" placeholder="Your Message Here *" rows="4" required="required" data-error="Please include a message."></textarea>
+								<div class="help-block with-errors"></div>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<input type="submit" class="btn btn-success btn-send" value="Send message">
+						</div>
+					</div>
+
 				</div>
-
-				<div class="text-center py-1">
-					<input type="email" placeholder="Email:">
-				</div>
-
-				<div class="text-center py-1">
-					<input type="tel" placeholder="Phone Number:">
-				</div>
-
-				<div class="text-center py-1">
-					<textarea name="message" cols="30" rows="10" placeholder="Message:"></textarea>
-				</div>
-
-				<hr>
-
-				<button type="button" class="btn btn-primary mb-4">Submit</button>
 
 			</form>
 		</div>
 
 
 		<!-- Footer -->
-		<footer class="page-footer font-small cyan darken-3 mt-1 bg-dark">
+		<footer class="page-footer font-small cyan darken-3 mt-4 bg-dark">
 
 			<!-- Footer Links -->
 			<div class="container">
